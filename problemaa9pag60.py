67e93697df0318765df8b50e8f4d1265ca49af96
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2021-11-17 19:55:05 +0200

    Delete problemaa9pag60.py

--- problemaa9pag60.py
+++
@@ -1,9 +0,0 @@
-A = set(str(input('Dati multimea A:')))
-B = set(str(input('Dati multimea B:')))
-if (all(x.isupper() for x in A)) and (all(x.isupper() for x in B)) == True:
-    print('Intersectia multimilor A si B -> ', A.intersection(B))
-    print('Reuniunea multimilor A si B -> ', A.union(B))
-    print('Diferenta multimilor A si B -> ', A.difference(B))
-    print('Diferenta multimilor B si A -> ', B.difference(A))
-else:
-    print('Toate elementele trebuie sa fie litere mari')
