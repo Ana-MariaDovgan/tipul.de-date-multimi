ab8d13a99ddcc18265093f842ce4d4962bd20d9a
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2021-11-17 19:12:33 +0200

    Add files via upload

---
+++ Problema8pag60.py
@@ -0,0 +1,8 @@
+A={1,2,3,4}
+B={3,4,5,6}
+print(A)
+print(B)
+print('Intersectia multimilor->',A.intersection(B))
+print('Reuniunea multimilor->',A.union(B))
+print('Diferenta multimilor->',A.difference(B))
+print('Diferenta multimilor->',B.difference(A))
