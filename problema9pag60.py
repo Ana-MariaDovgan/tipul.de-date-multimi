fbaca23502f2387e5990b9d55106afe4f063bc25
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2021-11-17 19:13:09 +0200

    Delete problema9pag60.py

--- problema9pag60.py
+++
@@ -1,8 +0,0 @@
-A={L,B,C,D}
-B={C,D,E,F}
-print(A)
-print(B)
-print('Intersectia multimilor->',A.intersection(B))
-print('Reuniunea multimilor->',A.union(B))
-print('Diferenta multimilor->',A.difference(B))
-print('Diferenta multimilor->',B.difference(A))
